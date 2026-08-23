ff3f4dc6cbbb0ee3985d5ce5e23084be0770d8e6
zukor <turcotte@zukor.com>
2026-08-23 00:56:48 -0500

    add animated Photo Notes header logo

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'efc-shell-v26';
-const SHELL = ['/', '/index.html', '/app.js', '/styles.css', '/manifest.json', '/logo.svg', '/icon.svg', '/zukor-logo.svg', '/send.js'];
+const CACHE = 'efc-shell-v27';
+const SHELL = ['/', '/index.html', '/app.js', '/styles.css', '/manifest.json', '/logo.svg', '/logo-animated.svg', '/icon.svg', '/zukor-logo.svg', '/send.js'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
