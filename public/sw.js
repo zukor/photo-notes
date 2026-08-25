304491e33fc3e0464d388d6e2d9c5f78b14aaec8
zukor <turcotte@zukor.com>
2026-08-25 10:17:51 -0500

    Bust header style cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'efc-shell-v60';
+const CACHE = 'efc-shell-v61';
 const SHELL = ['/', '/index.html', '/app.js?v=59', '/styles.css?v=60', '/manifest.json', '/logo.svg', '/logo-animated.svg', '/asphalt-pro-logo-animated.svg?v=57', '/icon.svg', '/zukor-logo.svg', '/send.js?v=59', '/vendor/html2canvas.min.js?v=59'];
 
 self.addEventListener('install', (e) => {
